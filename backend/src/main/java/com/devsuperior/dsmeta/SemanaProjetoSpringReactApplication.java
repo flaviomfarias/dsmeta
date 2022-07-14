9944bf1c930a2d399daf76c761e27cf300256037
flavio farias <flaviomfarias@yahoo.com.br>
2022-07-13 22:01:06 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/SemanaProjetoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaProjetoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaProjetoSpringReactApplication.class, args);
+	}
+
+}
