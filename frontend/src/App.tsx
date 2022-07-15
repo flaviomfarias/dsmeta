acc31bb3ae86d916fbdc7940e762b34903fca320
flavio farias <flaviomfarias@yahoo.com.br>
2022-07-15 10:28:44 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-
+import NotificationButton from "./componets/NotificationButton"
 
 function App() {
 
   return (
+    <> 
     <h1>ola mundo</h1>
+    <NotificationButton />
+    </>
   )
 
  
---
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -0,0 +1,17 @@
+
+import icon from "../../assets/ing/notification.icon.svg"
+import "./style.css"
+
+function notificationbutton() {
+
+    return (
+        <div className="dsmeta-red-btn">
+            <img src= {icon} alt="Notificar" />
+        </div>
+    )
+
+
+}
+
+
+export default notificationbutton
